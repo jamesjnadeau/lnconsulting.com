67eaff4a63c54ec50a71edced247e2e0fe207dde
jamesjnadeau <james.nadeau@gmail.com>
2014-11-05 17:25:00 +0000

    changing site title

--- config.php
+++ config.php
@@ -23,7 +23,7 @@ $config['custom_setting'] = 'Hello'; 	// Can be accessed by {{ config.custom_set
 */
 header("Cache-Control: max-age=7200");
 
-$config['site_title'] = 'L.N. Consulting Inc.';
+$config['site_title'] = 'LN Consulting';
 //$config['theme'] = 'jqm_1.3.2';
 $config['theme'] = 'bootstrap';
 $config['twig_config'] = array(			// Twig settings
