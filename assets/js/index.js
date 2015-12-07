7482a14b52c46871b2c869c77f043382d731c002
jamesjnadeau <james.nadeau@gmail.com>
2015-12-07 20:27:10 +0000

    fixing bad link

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,5 @@
 require('./jquery.smoothState.js');
-//Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-						})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-56034285-1', 'auto');
-ga('send', 'pageview');
-var ga_track_page = function()
-{
-	hash = location.hash;
-	try
-	{
-		if (hash)
-			ga('send', '_trackPageview', hash.substr(1));
-		else
-			ga('send', '_trackPageview', location.pathname);
-	}
-	catch(err)
-	{
-		console.log('unable to track page', err);
-	}
-};
+
 //this does the page transitions
 $(document).ready(function()
 				  {
@@ -68,3 +47,26 @@ $(document).ready(function()
 	}
 	fix_missing_images();
 });
+
+//Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+						})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-56034285-1', 'auto');
+ga('send', 'pageview');
+var ga_track_page = function()
+{
+	hash = location.hash;
+	try
+	{
+		if (hash)
+			ga('send', '_trackPageview', hash.substr(1));
+		else
+			ga('send', '_trackPageview', location.pathname);
+	}
+	catch(err)
+	{
+		console.log('unable to track page', err);
+	}
+};
