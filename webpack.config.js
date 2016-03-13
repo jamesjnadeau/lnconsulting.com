80ef8537bbfe687714993f84e33fe2dba470195c
jamesjnadeau <james.nadeau@gmail.com>
2016-03-13 08:38:30 -0400

    moving to jade loader, add isotop filtering, seperate higher Ed

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,12 @@ var marked = require('marked');
 var jade = require('jade');
 var minify = require('html-minifier').minify;
 var minifyOptions = {
-	removeComments: true,
-	collapseWhitespace: true,
-	collapseBooleanAttributes: true,
-	removeAttributeQuotes: true,
-	removeEmptyAttributes: true,
-	//removeEmptyElements: true,
+  removeComments: true,
+  collapseWhitespace: true,
+  collapseBooleanAttributes: true,
+  removeAttributeQuotes: true,
+  removeEmptyAttributes: true,
+  //removeEmptyElements: true,
 }
 
 
@@ -19,7 +19,7 @@ var minifyOptions = {
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
-
+var notJadeContent = [];
 module.exports = {
   //enable source-maps
   devtool: 'source-map',
@@ -27,7 +27,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.html$/, loader: "html-loader" },
-      { test: /\.css$/, 
+      { test: /\.css$/,
        //loader: "style-loader!css-loader"
        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
       },
@@ -79,7 +79,7 @@ module.exports = {
       //watch the content directory for changes
       this.addContextDependency(path);
 
-	  //Define our template path
+    //Define our template path
       var templatePath = 'templates/default.jade';
 
       //watch the template for changes
@@ -90,39 +90,60 @@ module.exports = {
     },
     //Test if a file should be processed or not, should return true or false;
     testToInclude: function(path, stats, absPath) {
-      return pathUtil.extname(path) === '.md';
+      return pathUtil.extname(path) === '.md' || pathUtil.extname(path) === '.jade';
     },
     //allows you to rewrite the url path that this will be uploaded to
     rewriteUrlPath: function(path, stats, absPath) {
+      var extensionSize;
+      if (pathUtil.extname(path) === '.md') {
+        extensionSize = -3;
+      } else {
+        extensionSize = -5;
+        this.addDependency(absPath);
+      }
+
       //strip out the extension
-      var urlPath = path.slice(0, -3);
+      var urlPath = path.slice(0, extensionSize);
 
       //rewrite /index to be just /, making index.md files become the folder index properly
       urlPath = urlPath.replace('index', '');
 
+      //store these for later
+      if (extensionSize === -3) {
+        notJadeContent.push(urlPath);
+      }
+
       return urlPath;
     },
+
     processFile: function(file, content) {
-      //Assemeble some meta data to use in template
+      if (pathUtil.extname(file.absPath) === '.md') { //this is a regular markdown file
+        //Assemeble some meta data to use in template
+        //match pico header info
+        //see https://github.com/picocms/Pico/blob/v1.0.0-beta.2/lib/Pico.php#L760
+        var picoCMSMetaPattern = /^\/\*(([\s\S])*?)\*\//;
+        var meta = {};
+        var temp = content.match(picoCMSMetaPattern);
+        temp[1].split(/\r?\n/).forEach(function(value) {
+          var row = value.split(':');
+          meta[row[0]] = row[1];
+        });
 
-      //match pico header info
-      //see https://github.com/picocms/Pico/blob/v1.0.0-beta.2/lib/Pico.php#L760
-      var picoCMSMetaPattern = /^\/\*(([\s\S])*?)\*\//;
-      var meta = {};
-      var temp = content.match(picoCMSMetaPattern);
-      temp[1].split(/\r?\n/).forEach(function(value){
-        var row = value.split(':');
-        meta[row[0]] = row[1];
-      });
+        //use compiled template to produce html file
+        var fileContents = this.template({
+          title: meta.Title,
+          description: meta.Description,
+          content: marked(content.replace(picoCMSMetaPattern, '')),
+        });
 
-      //use compiled template to produce html file
-      var fileContents = this.template({
-        title: meta.Title,
-        description: meta.Description,
-        content: marked(content.replace(picoCMSMetaPattern, ''))
-      });
-
-      return minify(fileContents, minifyOptions);
+        return fileContents;
+      } else {
+        // new jade file type
+        return jade.render(content, {
+          pretty: false,
+          filename: file.absPath,
+        });
+      }
     },
-  }
+  },
 };
